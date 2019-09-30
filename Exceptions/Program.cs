f3b4154d842f475fec59fcde3ba0808c47632e81
Lasma <lasma.stanke@gmail.com>
2019-09-30 20:55:40 +0300

    Added exception

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,7 +10,49 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int num1 = GetNumber("Enter number A: ");
+                int num2 = GetNumber("Enter number B: ");
+                Console.WriteLine($"Division = {Calculator.Divide(num1, num2)}");
+            }
+            catch (CalculatorExceptions ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Neparedzēta kļūda!");                
+            }
+            
 
+            Console.Read();
+        }
+        static int GetNumber(string text)
+        {
+            Console.Write(text);
+            string input = Console.ReadLine();
+            //if (int.TryParse(input, out int result))
+            //{
+            //    return result;
+            //}
+            //else
+            //{
+            //    return GetNumber(text);
+            //}
+
+            try
+            {
+                int result = int.Parse(input);
+                return result;
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+                return GetNumber(text);
+            }
+            
         }
     }
 }
